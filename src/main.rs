15e20073efc84d8995d9c77d2e219efc9382521b
Morten Bergseng <bergseng.m@gmail.com>
2022-12-12 11:22:28 +0100

    some tests + health check function

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-use std::io::Result;
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use std::net::TcpListener;
+use zero2prod::run;
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:3000")?
-    .run()
-    .await
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+
+    run(listener)?.await
 }
